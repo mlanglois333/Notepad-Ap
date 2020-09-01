3dae35c44af1d82a66a327c5565e9488d9fd0f80
Marc <mlanglois333@icloud.com>
2020-09-01 16:46:50 -0400

    html routes created

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,2 +1,4 @@
 const fs = require("fs");
 const path = require("path");
+
+const dbData = require("../db/db.json");
--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,2 +1,16 @@
 const fs = require("fs");
 const path = require("path");
+
+const dbData = require("../db/db.json");
+
+module.exports = function(app) {
+
+    app.get("/notes", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+  
+    app.get("*", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+};
