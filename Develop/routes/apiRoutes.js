75227fa97637ff4f94db61a92895d8d88c47aef5
Marc <mlanglois333@icloud.com>
2020-09-05 08:50:07 -0400

    get and post working

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,18 +1,39 @@
 const fs = require("fs");
 const path = require("path");
+const express = require("express");
+var app = express();
+let notes = [];
 
-const dbData = require("../db/db.json");
 
+fs.readFile("../Develop/db/db.json", function( err, data){
+if (err) throw err;
+notes = JSON.parse(data);
+console.log(notes);
+});
+
+
+function saveNotes(){
+
+  let data = JSON.stringify(notes)
+
+  fs.writeFile("../Develop/db/db.json", data, (err) => {
+    if (err) throw err;
+  });
+}
+
+
+ 
 module.exports = function(app) {
 
     app.get("/api/notes", function(req, res) {
-        res.json(dbData);
+        res.json(notes);
       });
 
       app.post("/api/notes", function(req, res) {
 
-        dbData.push(req.body);
-        res.json(dbData);
+        notes.push(req.body);
+        res.json(notes);
+        saveNotes();
        
       });
 
